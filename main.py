714ca2551fe66b2fec7d1f42eacb9ef1250d83da
Meynrun <ahakahacker@mail.ru>
2025-02-19 21:46:53 +0300

    1.6 error handling update

--- main.py
+++ main.py
@@ -8,20 +8,20 @@ from shared import lang
 from auto_update import check_for_updates
 
 if __name__ == '__main__':
-    shared.title()
-
     try:
+        shared.title()
         check_for_updates()
         mode = "1" if input(lang["main.choose.mode"]) == "1" else "2"
+
+        shared.clear_console()
+        shared.title()
+
+        if mode == "1":
+            asyncio.run(server())
+        elif mode == "2":
+            asyncio.run(client())
+
+        input(lang["main.enterToExit"])
+
     except KeyboardInterrupt:
         exit()
-
-    shared.clear_console()
-    shared.title()
-
-    if mode == "1":
-        asyncio.run(server())
-    elif mode == "2":
-        asyncio.run(client())
-
-    input(lang["main.enterToExit"])

11f9579e11e18abd99172f0d4c76183151eeb4d7
Meynrun <ahakahacker@mail.ru>
2025-02-21 00:46:44 +0300

    hotfix

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 from colorama import Fore
 
-VERSION = "v1.7.2"
+VERSION = "v1.7.3"
 TITLE = f"""
 {Fore.LIGHTYELLOW_EX}ZapFiles {Fore.LIGHTWHITE_EX}{VERSION}
 {Fore.LIGHTYELLOW_EX}Made with 💖 by {Fore.LIGHTBLUE_EX}Meynrun

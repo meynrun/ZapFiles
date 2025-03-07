64eb187469a5ed9366d773063ea4d7e87d9b9346
Meynrun <ahakahacker@mail.ru>
2025-03-08 00:38:15 +0300

    Finally remove_emojis() removes all emojis (I hope so)

--- env.py
+++ env.py
@@ -1,12 +1,12 @@
 from colorama import Fore
 from configuration import config
 
-VERSION = "v1.9.0"
+VERSION = "v1.9.0-rc2"
 
 if config.get_value("enable_emojis"):
     TITLE = f"""{Fore.LIGHTYELLOW_EX}ZapFiles {Fore.LIGHTWHITE_EX}{VERSION}
-    {Fore.LIGHTYELLOW_EX}Made with 💖 by {Fore.LIGHTBLUE_EX}Meynrun
-    """
+{Fore.LIGHTYELLOW_EX}Made with 💖 by {Fore.LIGHTBLUE_EX}Meynrun
+"""
 else:
     TITLE = f"""{Fore.LIGHTYELLOW_EX}ZapFiles {Fore.LIGHTWHITE_EX}{VERSION}
 {Fore.LIGHTYELLOW_EX}Made with love by {Fore.LIGHTBLUE_EX}Meynrun

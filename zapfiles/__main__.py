b1d3774f4753be177eba74e04f6336f6fb2f71da
Meynrun <ahakahacker@mail.ru>
2025-08-17 00:23:30 +0300

    pip installation and setuptools build

--- zapfiles/__main__.py
+++ zapfiles/__main__.py
@@ -9,7 +9,6 @@ import questionary
 from zapfiles.cli import clear_console, title
 from zapfiles.client import client, connect
 from zapfiles.core.config.app_configuration import config
-from zapfiles.core.config.experiments_configuration import experiments_config
 from zapfiles.core.localization import lang
 from zapfiles.core.updater import check_for_updates
 from zapfiles.server import server
@@ -40,49 +39,36 @@ def main() -> None:
             "⚡ZapFiles" if config.get_value("enable_emojis") else "ZapFiles"
         )
 
-    try:
-        clear_console()
-        title()
+    clear_console()
+    title()
 
-        if (
-            asyncio.run(handle_zapfile()) == 1
-        ):  # if argv[1] was a valid zapfile then return
-            return
+    if asyncio.run(handle_zapfile()) == 1:  # if argv[1] was a valid zapfile then return
+        return
 
-        if config.get_value("check_for_updates"):
-            check_for_updates()
+    if config.get_value("check_for_updates"):
+        check_for_updates()
 
-        choices = [
-            {"name": lang.get_string("main.mode.host"), "value": "host"},
-            {"name": lang.get_string("main.mode.get"), "value": "get"},
-        ]
+    choices = [
+        {"name": lang.get_string("main.mode.host"), "value": "host"},
+        {"name": lang.get_string("main.mode.get"), "value": "get"},
+    ]
 
-        if "lan_broadcast" in experiments_config.get_enabled_experiments():
-            choices.append(
-                {
-                    "name": lang.get_string("experiments.lan_broadcast.scan_mode"),
-                    "value": "scan_lan",
-                }
-            )
+    mode = questionary.select(
+        message=lang.get_string("main.mode.select"), choices=choices
+    ).ask()
 
-        mode = questionary.select(
-            message=lang.get_string("main.mode.select"), choices=choices
-        ).ask()
+    clear_console()
+    title()
 
-        clear_console()
-        title()
-
-        if mode == "host":
-            asyncio.run(server())
-        elif mode == "get":
-            asyncio.run(client())
-        elif mode == "scan_lan":
-            print("Not implemented yet")
-
-    except KeyboardInterrupt:
-        sys.exit(0)
+    if mode == "host":
+        asyncio.run(server())
+    elif mode == "get":
+        asyncio.run(client())
 
 
 if __name__ == "__main__":
     main()
-    input(lang.get_string("main.enterToExit"))
+    try:
+        input(lang.get_string("main.enterToExit"))
+    except KeyboardInterrupt:
+        pass
